b6b9adb9cfbea087d867c6cb0e1744421c1164ae
Pierluigi <pierluigi.capirci89@gmail.com>
2024-05-02 15:12:06 +0100

    feat: create a search that change the url

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,6 +3,22 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams()
+   const pathname = usePathname();
+   const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+     console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams)
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+    console.log(term)
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
